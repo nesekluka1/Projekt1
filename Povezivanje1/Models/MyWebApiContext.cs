00ba4f6fe8f5df75f2b2bf7155fbae77f6d5b044
nesekluka1 <luka.nesek@gmail.com>
2019-12-10 17:20:36 +0100

    Initial commit

---
+++ Povezivanje1/Models/MyWebApiContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Povezivanje1.Models
+{
+    public class MyWebApiContext : DbContext
+    {
+        public MyWebApiContext(DbContextOptions<MyWebApiContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
---
+++ Povezivanje1/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Povezivanje1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        // from the group model (Entity framework will connect the Primarykey and forign key)
+        public Group Group { get; set; }
+        public int GroupId { get; set; }
+    }
+}
